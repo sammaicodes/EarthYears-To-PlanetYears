2505e6fe91560195732f3c1c5f788270795ac80b
Sammai Gutierrez <sammaigutierrez@gmail.com>
2021-04-16 10:46:10 -0700

    Fail first test.

---
+++ __tests__/galaxCalc.test.js
@@ -0,0 +1,9 @@
+import Galaxy from '../src/js/galaxCalc.js'
+
+describe('Galaxy', () => {
+  test('Should create an object with properties: planetName, usersAge,', () => {
+    let user = new Galaxy();
+    expect(user).toEqual(true);
+  })
+  
+});
---
+++ src/js/galaxCalc.js
@@ -0,0 +1,10 @@
+export default class Galaxy {
+  constructor(planetName, usersAge) {
+    
+  }
+
+  galacticAgeCalc() {
+    
+  }
+
+};
